fe4ae749849662a8e4dbf93fb5c66ad73e03bffc
tomlightning <tomer@cedato.com>
2022-06-26 13:19:52 +0300

    implementing Eyal text plus small ui changes

--- src/components/pages/congratulations/AfterMinting.js
+++ src/components/pages/congratulations/AfterMinting.js
@@ -9,11 +9,21 @@ import { useNavigate, useParams } from "react-router-dom";
 
 
     return (
-<div className="d-flex justify-content-center flex-column">
-    <h3 className="congrats-headline">Congratulations!</h3>
-    <div className="font-weight-bold m-2">You just joined the Esteroids family. What's next?</div>
-    <div className="m-2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</div>
-    <div><button  className="btn btn-secondary-wnft" onClick={() => navigate(`/keyphrases/${params.keyphraseId}`,{replace: true})} >Edit My Keyphrase</button></div>
+<div className="d-flex  flex-column">
+    <div className="h2 congrats-headline d-flex justify-content-center font-weight-bold">Congratulations!</div>
+    <div className="font-weight-bold m-4 d-flex justify-content-center">You just became an Esterodian. Do you feel any different?</div>
+    <div className="mt-1 mb-3 px-4">
+        <div className="m-2">Well, you SHOULD feel different. You just got yourself a piece of esteroids.eth. You just became part of the future web. </div>
+
+		<div className="m-2">We imagine a futuristic web of community platforms. These platforms are going to be for the people, and owned by the people. We imagine esteroids.eth as a community search engine. We want people to have part of it; to have a saying on decisions in it. To be partial owners of it.</div>
+
+		<div className="m-2">So yes, you didn't just got yourself a keyphrase; you also became a pioneer of the future web.</div>
+
+		<div className="m-2">Welcome to the future web; hold tight; it's gonna be an exciting ride. </div>
+    </div>
+    <div className="d-flex justify-content-center">
+        <button  className="btn btn-secondary-wnft" onClick={() => navigate(`/keyphrases/${params.keyphraseId}`,{replace: true})} >Edit My Keyphrase</button>
+    </div>
 </div>
         ) 
 }
